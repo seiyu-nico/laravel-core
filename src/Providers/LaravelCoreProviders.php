37f01fd76dc3f5370f3d90547db15138ad640bf6
seiyu-nico <yuseiishii141@gmail.com>
2023-01-25 22:26:54 +0900

    getOr追加

---
+++ src/Providers/LaravelCoreProviders.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace SeiyuNico\LaravelCore\Providers;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\ServiceProvider;
+
+class LaravelCoreProviders extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Builder::macro('getOr', function ($callback) {
+            /** @var \Illuminate\Database\Eloquent\Model $this */
+            return tap($this->get(), fn ($results) => ($results->isNotEmpty()) ? $results : call_user_func($callback));
+        });
+    }
+}
