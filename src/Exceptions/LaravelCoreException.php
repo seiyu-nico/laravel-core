f94cec8356d4d7eb777c445df0e94ea65130bc3b
Yusei Ishii <37203978+seiyu-nico@users.noreply.github.com>
2023-01-26 00:33:57 +0900

    Update LaravelCoreException.php

--- src/Exceptions/LaravelCoreException.php
+++ src/Exceptions/LaravelCoreException.php
@@ -7,7 +7,7 @@ use Throwable;
 
 class LaravelCoreException extends Exception
 {
-    public function __construct(string $message = '', int $code = 422, ?Throwable $previous = null)
+    public function __construct(string $message = '', int $code = 500, ?Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
